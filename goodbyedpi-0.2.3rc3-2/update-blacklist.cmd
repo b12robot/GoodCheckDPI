a863d02765925317a510aa28aba272d0dccae434
B12 <87882740+b12robot@users.noreply.github.com>
2025-08-05 15:17:35 +0300

    Update update-blacklist.cmd

--- goodbyedpi-0.2.3rc3-2/update-blacklist.cmd
+++ goodbyedpi-0.2.3rc3-2/update-blacklist.cmd
@@ -1,3 +1,3 @@
 @echo off
-curl -o "%~dp0test-blacklist.txt" ""
+curl -o "%~dp0turkey-blacklist.txt" "https://raw.githubusercontent.com/b12robot/GoodCheckDPI/refs/heads/main/goodbyedpi-0.2.3rc3-2/turkey-blacklist.txt"
 exit
